c3f58adca9ce02f79f11ef58a9a9f0f8f4107a38
lab302-28 <lab302-28@labeng.local>
2019-01-11 16:37:55 +0700

    hello

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -2,13 +2,7 @@
 using namespace std;
 
 int main(){
-    float A = 2, B = 69,i = A, ans = 0;
-    while(i <= B){
-        ans = ans + 1/i;
-        i = 1 / i+1;
-    }
-    cout << "ans = " << ans;
-
-    return 0;
+    cout << "hello";
+    
 }
     
