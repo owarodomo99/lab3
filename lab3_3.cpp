b957d0e63bf95d20db2a27fc214cb1956036fc5d
lab302-28 <lab302-28@labeng.local>
2019-01-11 16:22:26 +0700

    3.3-1

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -7,5 +7,14 @@ int main(){
     cin >> A;
     cout << "B";
     cin >> B;
+    int i = N, ans = 0;
+    while(i <= M){
+        ans = ans + i;
+        i = 1 / i+1;
+    }
+    cout << "ans = " << ans;
+
+    return 0;
+}
     
 }
